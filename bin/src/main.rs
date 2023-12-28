f19818dde15c8c0cacbeac2da0fe9dcb7df7402b
Leo Alt <leo@ethereum.org>
2023-12-28 13:40:43 +0100

    Use serde data and update revm

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,5 +1,5 @@
-use powdr::number::{FieldElement, GoldilocksField};
-use powdr::pipeline::Pipeline;
+use powdr::number::GoldilocksField;
+use powdr::pipeline::{Pipeline, Stage};
 use powdr::riscv::continuations::{
     bootloader::default_input, rust_continuations, rust_continuations_dry_run,
 };
@@ -9,22 +9,10 @@ use powdr::riscv_executor;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 
-use models::*;
-
 use thiserror::Error;
 use walkdir::{DirEntry, WalkDir};
 
-use revm::{
-    db::{CacheDB, CacheState, EmptyDB},
-    interpreter::CreateScheme,
-    primitives::{
-        address, b256, calc_excess_blob_gas, keccak256, ruint::Uint, AccountInfo, Address,
-        Bytecode, Bytes, Env, HashMap, SpecId, TransactTo, B256, U256,
-    },
-    EVM,
-};
-
-use std::collections::HashMap as STDHashMap;
+use revm::primitives::B256;
 
 fn main() {
     env_logger::init();
@@ -36,7 +24,7 @@ fn eth_test_simple() {
     let eth_tests_path = Path::new("../ethereum-tests/simple");
     //let eth_tests_path = Path::new("../ethereum-tests/GeneralStateTests/VMTests");
     //let eth_tests_path = Path::new("../ethereum-tests/long");
-    let all_tests = find_all_json_tests(&eth_tests_path);
+    let all_tests = find_all_json_tests(eth_tests_path);
     println!("{all_tests:?}");
 
     println!("Compiling Rust...");
@@ -50,39 +38,45 @@ fn eth_test_simple() {
     .ok_or_else(|| vec!["could not compile rust".to_string()])
     .unwrap();
 
+    //println!("{asm_contents}");
+
     let mk_pipeline = || {
         Pipeline::<GoldilocksField>::default()
             .from_asm_string(asm_contents.clone(), Some(asm_file_path.clone()))
+            .with_prover_inputs(vec![])
     };
 
-    //println!("{asm_contents}");
+    println!("Creating pipeline from powdr-asm...");
+    let start = Instant::now();
+    let pipeline = mk_pipeline();
+    let duration = start.elapsed();
+    println!("Pipeline from powdr-asm took: {:?}", duration);
+
+    println!("Advancing pipeline to fixed columns...");
+    let start = Instant::now();
+    let pil_with_evaluated_fixed_cols = pipeline.pil_with_evaluated_fixed_cols().unwrap();
+    let duration = start.elapsed();
+    println!("Advancing pipeline took: {:?}", duration);
 
     for t in all_tests {
         println!("Running test {}", t.display());
-        //let suite = read_suite(&t);
 
         println!("Reading JSON test...");
         let suite_json = std::fs::read_to_string(&t).unwrap();
-        println!("Creating data callback...");
-        let mut suite_json_bytes: Vec<GoldilocksField> = suite_json
-            .into_bytes()
-            .iter()
-            .map(|b| (*b as u32).into())
-            .collect();
-        suite_json_bytes.insert(0, (suite_json_bytes.len() as u32).into());
 
-        let mut data: STDHashMap<GoldilocksField, Vec<GoldilocksField>> = STDHashMap::default();
-        data.insert(666.into(), suite_json_bytes);
+        let mk_pipeline_with_data = || mk_pipeline().add_data(42, &suite_json);
 
-        let output_dir = Path::new("/tmp/test");
-        let force_overwrite = true;
+        let mk_pipeline_opt = || {
+            mk_pipeline_with_data()
+                .from_pil_with_evaluated_fixed_cols(pil_with_evaluated_fixed_cols.clone())
+        };
 
         println!("Running powdr-riscv executor in fast mode...");
         let start = Instant::now();
-        let (trace, mem) = riscv_executor::execute::<GoldilocksField>(
+        let (trace, _mem) = riscv_executor::execute::<GoldilocksField>(
             &asm_contents,
-            &data,
-            &default_input(),
+            mk_pipeline_with_data().data_callback().unwrap(),
+            &default_input(&[]),
             riscv_executor::ExecMode::Fast,
         );
         let duration = start.elapsed();
@@ -91,80 +85,29 @@ fn eth_test_simple() {
 
         println!("Running powdr-riscv executor in trace mode for continuations...");
         let start = Instant::now();
-        let bootloader_inputs = rust_continuations_dry_run(mk_pipeline(), data.clone());
+        let bootloader_inputs = rust_continuations_dry_run(mk_pipeline_with_data());
         let duration = start.elapsed();
         println!("Trace executor took: {:?}", duration);
 
-        /*
-        println!("Compiling powdr-asm...");
-        let _result = compile_asm_string_with_callback(
-            asm_file_path.to_str().unwrap(),
-            &asm_contents,
-            data_to_query_callback(data),
-            None,
-            output_dir,
-            force_overwrite,
-            None,
-            vec![],
-        ).unwrap();
-        */
-        println!("Done.");
+        let generate_witness =
+            |mut pipeline: Pipeline<GoldilocksField>| -> Result<(), Vec<String>> {
+                let start = Instant::now();
+                println!("Generating witness...");
+                pipeline.advance_to(Stage::GeneratedWitness)?;
+                let duration = start.elapsed();
+                println!("Generating witness took: {:?}", duration);
+                Ok(())
+            };
 
-        /*
-        for (name, unit) in suite.0 {
-            println!("{name}");
-        }
-        */
+        println!("Running witness generation...");
+        let start = Instant::now();
+        rust_continuations(mk_pipeline_opt, generate_witness, bootloader_inputs).unwrap();
+        let duration = start.elapsed();
+        println!("Witness generation took: {:?}", duration);
+
+        println!("Done.");
     }
 }
-
-/*
-static BYTECODE: &str = "61029a60005260206000f3";
-fn simple_test() {
-    let output_dir = Path::new("/tmp/test");
-    let force_overwrite = true;
-
-    println!("Compiling Rust...");
-    let (asm_file_path, asm_contents) =
-        compile_rust("./evm", Path::new("/tmp/test"), true, &CoProcessors::base(), false)
-        .ok_or_else(|| vec!["could not compile rust".to_string()]).unwrap();
-
-    let bytes = hex::decode(BYTECODE).unwrap();
-    let bytecode_len = bytes.len();
-    let mut input: Vec<GoldilocksField> = bytes.into_iter().map(|x| (x as u64).into()).collect();
-    input.insert(0, (bytecode_len as u64).into());
-
-    let mut data: HashMap<usize, Vec<GoldilocksField>> = HashMap::default();
-    data.insert(0, input);
-
-    println!("Compiling powdr-asm...");
-
-    /*
-    let _result = compile_asm_string(
-        asm_file_path.to_str().unwrap(),
-        &asm_contents,
-        input,
-        output_dir,
-        force_overwrite,
-        None,
-        vec![],
-    ).unwrap();
-    */
-
-    let _result = compile_asm_string_with_callback(
-        asm_file_path.to_str().unwrap(),
-        &asm_contents,
-        data_to_query_callback(data),
-        None,
-        output_dir,
-        force_overwrite,
-        None,
-        vec![],
-    ).unwrap();
-
-    println!("Done.");
-}
-*/
 
 fn find_all_json_tests(path: &Path) -> Vec<PathBuf> {
     WalkDir::new(path)
@@ -173,40 +116,6 @@ fn find_all_json_tests(path: &Path) -> Vec<PathBuf> {
         .filter(|e| e.file_name().to_string_lossy().ends_with(".json"))
         .map(DirEntry::into_path)
         .collect::<Vec<PathBuf>>()
-}
-
-fn data_to_query_callback<T: FieldElement>(
-    data: HashMap<usize, Vec<T>>,
-) -> impl Fn(&str) -> Option<T> {
-    move |query: &str| -> Option<T> {
-        let items = query.split(',').map(|s| s.trim()).collect::<Vec<_>>();
-        match items[0] {
-            "\"input\"" => {
-                assert_eq!(items.len(), 2);
-                let index = items[1].parse::<usize>().unwrap();
-                // 0 = "input"
-                let values = &data[&0];
-                values.get(index).cloned()
-            }
-            "\"data\"" => {
-                assert_eq!(items.len(), 3);
-                let index = items[1].parse::<usize>().unwrap();
-                let what = items[2].parse::<usize>().unwrap();
-                let values = &data[&what];
-                values.get(index).cloned()
-            }
-            "\"print_char\"" => {
-                assert_eq!(items.len(), 2);
-                print!("{}", items[1].parse::<u8>().unwrap() as char);
-                Some(0.into())
-            }
-            "\"hint\"" => {
-                assert_eq!(items.len(), 2);
-                Some(T::from_str(items[1]))
-            }
-            _ => None,
-        }
-    }
 }
 
 #[derive(Debug, Error)]
--- evm/src/lib.rs
+++ evm/src/lib.rs
@@ -1,94 +1,35 @@
 #![no_std]
 
 use revm::{
-    db::{CacheDB, EmptyDB, CacheState},
+    db::CacheState,
     interpreter::CreateScheme,
-    primitives::{
-        address, b256, calc_excess_blob_gas, keccak256, Env, HashMap, SpecId, ruint::Uint, AccountInfo, Address, Bytecode, Bytes, TransactTo, B256, U256,
-    },
-    EVM,
+    primitives::{calc_excess_blob_gas, keccak256, Bytecode, Env, SpecId, TransactTo, U256},
+    Evm,
 };
-//use runtime::{print, get_prover_input, coprocessors::{get_data, get_data_len}};
-use powdr_riscv_rt::{print, get_prover_input, coprocessors::{get_data, get_data_len}};
+
+use powdr_riscv_rt::{coprocessors::get_data_serde, print};
 
 use models::*;
 
+mod utils;
+
+use utils::recover_address;
+
 extern crate alloc;
-use alloc::vec::Vec;
-use alloc::vec;
 use alloc::string::String;
 use alloc::string::ToString;
+use alloc::vec::Vec;
 
 #[no_mangle]
 fn main() {
-    ethereum_tests_simple();
+    ethereum_test();
 }
 
-fn ethereum_tests_simple() {
-    let suite_len = get_data_len(666);
-    let mut suite_json = vec![0; suite_len];
-    get_data(666, &mut suite_json);
+fn ethereum_test() {
+    let suite_json: String = get_data_serde(42);
+    let suite = read_suite(&suite_json);
 
-    let suite_json: Vec<u8> = suite_json.into_iter().map(|x| x as u8).collect();
-    
-    let suite_json_str = String::from_utf8(suite_json).unwrap();
-    let suite = read_suite(&suite_json_str);
-
-    assert!(execute_test(&suite).is_ok());
-}
-
-fn simple_test() {
-    const CONTRACT_ADDR: Address = address!("0d4a11d5EEaaC28EC3F61d100daF4d40471f1852");
-    const CODE_HASH: B256 =
-        b256!("e3c84e69bac71c159b2ff0d62b9a5c231887a809a96cb4a262a4b96ed78a1db2");
-    let mut db = CacheDB::new(EmptyDB::default());
-
-    let bytecode_len = get_prover_input(0);
-    let bytecode: Vec<_> = (1..(bytecode_len + 1)).map(|idx| get_prover_input(idx) as u8).collect();
-    /*
-    let bytecode_len = get_data_len(0);
-    let mut bytecode = vec![0; bytecode_len];
-    get_data(0, &mut bytecode);
-    */
-
-    // Fill database:
-    let bytecode = Bytes::from(bytecode);
-    let account = AccountInfo::new(Uint::from(10), 0, CODE_HASH, Bytecode::new_raw(bytecode));
-
-    db.insert_account_info(CONTRACT_ADDR, account);
-
-    let mut evm: EVM<CacheDB<EmptyDB>> = EVM::new();
-    evm.database(db);
-
-    // fill in missing bits of env struc
-    // change that to whatever caller you want to be
-    evm.env.tx.caller = Address::from_slice(&[0; 20]);
-    // account you want to transact with
-    evm.env.tx.transact_to = TransactTo::Call(CONTRACT_ADDR);
-    // calldata formed via abigen
-    evm.env.tx.data = Bytes::new();
-    // transaction value in wei
-    evm.env.tx.value = U256::try_from(0).unwrap();
-
-    let result = evm.transact().unwrap();
-
-    match result.result {
-        revm::primitives::ExecutionResult::Success {
-            reason: _,
-            gas_used: _,
-            gas_refunded: _,
-            logs: _,
-            output,
-        } => print!("Success: {:#?}", output.into_data()),
-        revm::primitives::ExecutionResult::Revert {
-            gas_used: _,
-            output: _,
-        } => panic!("Revert!"),
-        revm::primitives::ExecutionResult::Halt {
-            reason: _,
-            gas_used: _,
-        } => panic!("Halt!"),
-    };
+    assert!(execute_test_suite(suite).is_ok());
 }
 
 fn read_suite(s: &String) -> TestSuite {
@@ -96,46 +37,18 @@ fn read_suite(s: &String) -> TestSuite {
     suite
 }
 
-fn execute_test(suite: &TestSuite) -> Result<(), String> {
-    let map_caller_keys: HashMap<_, _> = [
-        (
-            b256!("45a915e4d060149eb4365960e6a7a45f334393093061116b197e3240065ff2d8"),
-            address!("a94f5374fce5edbc8e2a8697c15331677e6ebf0b"),
-        ),
-        (
-            b256!("c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4"),
-            address!("cd2a3d9f938e13cd947ec05abc7fe734df8dd826"),
-        ),
-        (
-            b256!("044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d"),
-            address!("82a978b3f5962a5b0957d9ee9eef472ee55b42f1"),
-        ),
-        (
-            b256!("6a7eeac5f12b409d42028f66b0b2132535ee158cfda439e3bfdd4558e8f4bf6c"),
-            address!("c9c5a15a403e41498b6f69f6f89dd9f5892d21f7"),
-        ),
-        (
-            b256!("a95defe70ebea7804f9c3be42d20d24375e2a92b9d9666b832069c5f3cd423dd"),
-            address!("3fb1cd2cd96c6d5c0b5eb3322d807b34482481d4"),
-        ),
-        (
-            b256!("fe13266ff57000135fb9aa854bbfe455d8da85b21f626307bf3263a0c2a8e7fe"),
-            address!("dcc5ba93a1ed7e045690d722f2bf460a51c61415"),
-        ),
-    ]
-    .into();
-
-    for (name, unit) in &suite.0 {
+fn execute_test_suite(suite: TestSuite) -> Result<(), String> {
+    for (_name, unit) in suite.0 {
         // Create database and insert cache
         let mut cache_state = CacheState::new(false);
-        for (address, info) in &unit.pre {
+        for (address, info) in unit.pre {
             let acc_info = revm::primitives::AccountInfo {
                 balance: info.balance,
                 code_hash: keccak256(&info.code),
-                code: Some(Bytecode::new_raw(info.code.clone())),
+                code: Some(Bytecode::new_raw(info.code)),
                 nonce: info.nonce,
             };
-            cache_state.insert_account_with_storage(*address, acc_info, info.storage.clone());
+            cache_state.insert_account_with_storage(address, acc_info, info.storage);
         }
 
         let mut env = Env::default();
@@ -151,7 +64,7 @@ fn execute_test(suite: &TestSuite) -> Result<(), String> {
         env.block.basefee = unit.env.current_base_fee.unwrap_or_default();
         env.block.difficulty = unit.env.current_difficulty;
         // after the Merge prevrandao replaces mix_hash field in block and replaced difficulty opcode in EVM.
-        env.block.prevrandao = Some(unit.env.current_difficulty.to_be_bytes().into());
+        env.block.prevrandao = unit.env.current_random;
         // EIP-4844
         if let (Some(parent_blob_gas_used), Some(parent_excess_blob_gas)) = (
             unit.env.parent_blob_gas_used,
@@ -165,8 +78,11 @@ fn execute_test(suite: &TestSuite) -> Result<(), String> {
         }
 
         // tx env
-        let pk = unit.transaction.secret_key;
-        env.tx.caller = map_caller_keys.get(&pk).copied().ok_or_else(|| String::new())?;
+        env.tx.caller = match unit.transaction.sender {
+            Some(address) => address,
+            _ => recover_address(unit.transaction.secret_key.as_slice())
+                .ok_or_else(|| String::new())?,
+        };
         env.tx.gas_price = unit
             .transaction
             .gas_price
@@ -174,11 +90,11 @@ fn execute_test(suite: &TestSuite) -> Result<(), String> {
             .unwrap_or_default();
         env.tx.gas_priority_fee = unit.transaction.max_priority_fee_per_gas;
         // EIP-4844
-        env.tx.blob_hashes = unit.transaction.blob_versioned_hashes.clone();
+        env.tx.blob_hashes = unit.transaction.blob_versioned_hashes;
         env.tx.max_fee_per_blob_gas = unit.transaction.max_fee_per_blob_gas;
 
         // post and execution
-        for (spec_name, tests) in &unit.post {
+        for (spec_name, tests) in unit.post {
             if matches!(
                 spec_name,
                 SpecName::ByzantiumToConstantinopleAt5
@@ -188,9 +104,9 @@ fn execute_test(suite: &TestSuite) -> Result<(), String> {
                 continue;
             }
 
-            env.cfg.spec_id = spec_name.to_spec_id();
+            let spec_id = spec_name.to_spec_id();
 
-            for (index, test) in tests.into_iter().enumerate() {
+            for (_index, test) in tests.into_iter().enumerate() {
                 env.tx.gas_limit = unit.transaction.gas_limit[test.indexes.gas].saturating_to();
 
                 env.tx.data = unit
@@ -227,38 +143,29 @@ fn execute_test(suite: &TestSuite) -> Result<(), String> {
 
                 let mut cache = cache_state.clone();
                 cache.set_state_clear_flag(SpecId::enabled(
-                    env.cfg.spec_id,
+                    spec_id,
                     revm::primitives::SpecId::SPURIOUS_DRAGON,
                 ));
                 let mut state = revm::db::State::builder()
                     .with_cached_prestate(cache)
                     .with_bundle_update()
                     .build();
-                let mut evm = revm::new();
-                evm.database(&mut state);
-                evm.env = env.clone();
+                let mut evm = Evm::builder()
+                    .with_db(&mut state)
+                    .modify_env(|e| *e = env.clone())
+                    .spec_id(spec_id)
+                    .build();
 
                 // do the deed
-                let exec_result = evm.transact_commit();
+                //let timer = Instant::now();
+                let mut check = || {
+                    let exec_result = evm.transact_commit();
 
-                // validate results
-                // this is in a closure so we can have a common printing routine for errors
-                let check = || {
-                    // if we expect exception revm should return error from execution.
-                    // So we do not check logs and state root.
-                    //
-                    // Note that some tests that have exception and run tests from before state clear
-                    // would touch the caller account and make it appear in state root calculation.
-                    // This is not something that we would expect as invalid tx should not touch state.
-                    // but as this is a cleanup of invalid tx it is not properly defined and in the end
-                    // it does not matter.
-                    // Test where this happens: `tests/GeneralStateTests/stTransactionTest/NoSrcAccountCreate.json`
-                    // and you can check that we have only two "hash" values for before and after state clear.
                     match (&test.expect_exception, &exec_result) {
                         // do nothing
                         (None, Ok(_)) => (),
                         // return okay, exception is expected.
-                        (Some(_), Err(e)) => {
+                        (Some(_), Err(_e)) => {
                             //print!("ERROR: {e}");
                             return Ok(());
                         }
@@ -271,7 +178,6 @@ fn execute_test(suite: &TestSuite) -> Result<(), String> {
                     Ok(())
                 };
 
-                // dump state and traces if test failed
                 let Err(e) = check() else { continue };
 
                 return Err(e);
