f31662e9f1f79e94658dfa01fad135146f5e4bcf
eigmax <stephen@ieigen.com>
2024-01-21 01:50:46 +0800

    fix: REVM update

--- models/src/serializer.rs
+++ models/src/serializer.rs
@@ -6,6 +6,6 @@ pub fn serialize_u64_as_str<S>(v: &u64, serializer: S) -> Result<S::Ok, S::Error
 where
     S: Serializer,
 {
-    let sv = format!("{:x}", v);
+    let sv = format!("0x{:x}", v);
     serializer.serialize_str(&sv)
 }
